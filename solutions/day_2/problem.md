cac34fb331e3a282d8bc44d66759b7b3db1f7003
Wojciech Seweryn <wojciech.seweryn@shell.com>
2023-12-08 16:26:03 +0100

    adding day 2 solution

--- solutions/day_2/problem.md
+++ solutions/day_2/problem.md
@@ -15,3 +15,22 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 <p>In the example above, games 1, 2, and 5 would have been <em>possible</em> if the bag had been loaded with that configuration. However, game 3 would have been <em>impossible</em> because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been <em>impossible</em> because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get <code><em>8</em></code>.</p>
 <p>Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. <em>What is the sum of the IDs of those games?</em></p>
 </article>
+<article class="day-desc"><h2 id="part2">--- Part Two ---</h2><p>The Elf says they've stopped producing snow because they aren't getting any <em>water</em>! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!</p>
+<p>As you continue your walk, the Elf poses a second question: in each game you played, what is the <em>fewest number of cubes of each color</em> that could have been in the bag to make the game possible?</p>
+<p>Again consider the example games from earlier:</p>
+<pre style="position: relative;"><code>Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+</code><div class="open_grepper_editor" title="Edit &amp; Save To Grepper"></div></pre>
+<ul>
+<li>In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.</li>
+<li>Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.</li>
+<li>Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.</li>
+<li>Game 4 required at least 14 red, 3 green, and 15 blue cubes.</li>
+<li>Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.</li>
+</ul>
+<p>The <em>power</em> of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is <code>48</code>. In games 2-5 it was <code>12</code>, <code>1560</code>, <code>630</code>, and <code>36</code>, respectively. Adding up these five powers produces the sum <code><em>2286</em></code>.</p>
+<p>For each game, find the minimum set of cubes that must have been present. <em>What is the sum of the power of these sets?</em></p>
+</article>
--- solutions/day_2/solutions/wojtek_junior/solution.py
+++ solutions/day_2/solutions/wojtek_junior/solution.py
@@ -1,1 +1,233 @@
-#
+"""Solution to the day 2 of Advent of Code"""
+
+from __future__ import annotations
+
+import argparse
+import re
+from dataclasses import dataclass
+from typing import Iterable, Literal
+
+__author__ = "Wojtek Junior"
+
+INPUT = "input.txt"
+PART = Literal[1, 2]
+
+parser = argparse.ArgumentParser(description="Part of the daily problem")
+parser.add_argument(
+    "-p",
+    "--part",
+    default=1,
+    type=int,
+    help="Part of the daily problem - 1 or 2",
+)
+
+# game suffix regex - to extract id of the game
+SUFFIX = re.compile(r"^Game (\d+):")
+# cube color regex - to extract color of the cube from move
+COLOR = re.compile(r"(red|blue|green)")
+# number of cubes of the color regex - to extract number of cubes of color from move
+# extact the number form group 1 and color from group 2
+NUMBER = re.compile(r"(\d+) (red|blue|green)")
+
+
+@dataclass
+class CubeGame:
+    """
+    Class representing a single game of cubes.
+
+    Parameters
+    ----------
+    id : int
+        Id of the game.
+    moves : Iterable[CubesMove]
+        Iterable of moves in the game.
+    """
+
+    id: int
+    moves: Iterable[CubesMove]
+
+    def is_possible(self, config: CubesMove) -> bool:
+        """
+        Checks if the game is possible to happend (all moves are possible to perform)
+        if the configuration of cubes is given (with available number of cubes of each color).
+        Game is not possible if any of the moves requires more cubes of any color
+        than available in the configuration.
+
+        Parameters
+        ----------
+        config : CubesMove
+            CubesMove configuration to check against.
+
+        Returns
+        -------
+        bool
+            _description_
+        """
+        return all(move.is_possible(config) for move in self.moves)
+
+    @property
+    def power(self) -> int:
+        """
+        Calculates the power of the game.
+        The power is the product of the minimum number of cubes of each color in the game
+        to have to perform all the moves.
+
+        Returns
+        -------
+        int
+            Power of the game.
+        """
+        return (
+            max(move.blue for move in self.moves)
+            * max(move.red for move in self.moves)
+            * max(move.green for move in self.moves)
+        )
+
+
+@dataclass
+class CubesMove:
+    """
+    Class representing a single move of cubes.
+
+    Parameters
+    ----------
+    blue : int
+        Number of blue cubes.
+    green : int
+        Number of green cubes.
+    red : int
+        Number of red cubes.
+    """
+
+    blue: int = 0
+    green: int = 0
+    red: int = 0
+
+    def is_possible(self, config: CubesMove) -> bool:
+        """
+        Checks if the move is possible to perform if the configuration of cubes is given.
+        The move is not possible if it requires more cubes of any color
+        than available in the configuration.
+
+        Parameters
+        ----------
+        config : CubesMove
+            CubesMove configuration with available number
+            of cubes of each color to check against.
+
+        Returns
+        -------
+        bool
+            True if the move is possible to perform, False otherwise.
+        """
+        return (
+            self.blue <= config.blue and self.green <= config.green and self.red <= config.red
+        )
+
+
+def get_game_id(line: str) -> int:
+    """
+    Extracts id of the game from the line.
+
+    Parameters
+    ----------
+    line : str
+        Line of the input file with game description.
+
+    Returns
+    -------
+    int
+        Id of the game as int.
+
+    Raises
+    ------
+    ValueError
+        If the line does not contain a proper suffix.
+    """
+    suffix = SUFFIX.search(line)
+
+    if suffix is None:
+        raise ValueError(f"Invalid line without a suffix: {line}")
+
+    id_ = int(suffix.group(1))
+    return id_
+
+
+def get_move_dict(line: str) -> dict[str, int]:
+    """
+    Extracts dict with number of cubes of each color from the line.
+
+    Parameters
+    ----------
+    line : str
+        Line of the input file with move description.
+
+    Returns
+    -------
+    dict[str, int]
+        Dict with number of cubes of each color.
+    """
+    return {
+        COLOR.search(cube).group(0): int(NUMBER.search(cube).group(1))  # type: ignore
+        for cube in line.split(",")
+    }
+
+
+def get_game(line: str) -> CubeGame:
+    """
+    Extracts CubeGame from the line.
+
+    Parameters
+    ----------
+    line : str
+        Line of the input file with game description.
+
+    Returns
+    -------
+    CubeGame
+        CubeGame object representing the single game from line of input.
+    """
+    id_ = get_game_id(line)
+    moves = [CubesMove(**get_move_dict(move)) for move in line.split(";")]
+    return CubeGame(id=id_, moves=moves)
+
+
+def main(part: PART) -> int:
+    """
+    Calculates the solution to the problem from Day 2.
+
+    Parameters
+    ----------
+    part : PART
+        Part of the daily problem - 1 or 2.
+
+    Returns
+    -------
+    int
+        Solution to the problem.
+    """
+
+    with open(INPUT, "r") as f:
+        lines = f.readlines()
+
+    games = [get_game(line) for line in lines]
+
+    if part == 1:
+        return sum(game.id for game in games if game.is_possible(CONFIG))
+
+    elif part == 2:
+        return sum(game.power for game in games)
+
+    else:
+        raise ValueError(f"Invalid part of the problem: '{part}', must be 1 or 2.")
+
+
+# global configuration of cubes to check against - for 1 part of the problem
+# available number of cubes of each color - 14 blue, 13 green, 12 red
+CONFIG = CubesMove(blue=14, green=13, red=12)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    result = main(part=args.part)
+    print(result)
